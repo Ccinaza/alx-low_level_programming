c47ebb8581f2b67931e9da025d9eb92e316e1b04
Ccinaza <ccinaza@gmail.com>
2022-09-19 13:59:30 -0700

    PoArrStr

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int Naz;
+
+	Naz = *a;
+	*a = *b;
+	*b = Naz;
+}

b6795c1f734fff97fc4f236c11bf2a9d06396935
Ccinaza <ccinaza@gmail.com>
2022-12-12 15:34:30 -0800

    Libdynamic

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Check main
+ * @n: An integer input
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
